0b4d92d376b2f328b758e1bdb9ce76cf6fdb05fd
Kevin Krause <kkrausesc@gmail.com>
2017-09-23 17:55:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,7 +17,10 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  do {
-    array.pop
-  } while (array.length > 0 && maybeTrue())
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+    do {
+      array.pop
+    } while (array.length > 0 && maybeTrue())
 }
